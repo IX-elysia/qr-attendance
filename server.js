be739d3afa7efb938ae942e712cf066573deafac
thirdyjaybraga-droid <thirdyjaybraga@gmail.com>
2025-09-13 18:26:51 +0800

    added seperate qr generator for easier use

--- server.js
+++ server.js
@@ -97,3 +97,26 @@ app.get("/export", async (req, res) => {
 app.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
 );
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "client")));
+
+// Main page (scanner)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+// Generator page
+app.get("/generator", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "generator.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
